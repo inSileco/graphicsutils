4fde89a3b31a992d844549a9440be64da32b4426
Kevin Cazelles <kcazelle@uoguelph.ca>
2018-03-02 16:39:29 -0500

    additionnal unit testings for plot0 :gear:

--- R/plot0.R
+++ R/plot0.R
@@ -17,32 +17,35 @@
 #' @seealso \code{\link[shape]{emptyplot}}
 #'
 #' @examples
-#' # Example 1:
+#' # Example 1
 #' plot0()
 #'
-#' # Example 2:
+#' # Example 2
 #' plot0(c(-10,10), asp=1)
 #'
-#' # Example 2:
-#' plot0(c(-10,10), asp=1, fill=8, text='cool', srt=90, cex=2)
+#' # Example 3
+#' plot0(c(-10,10), text='cool', cex = 2)
+#'
+#' # Example 3
 #' plot0(c(-10,10), asp=1, fill=8, text='cool', srt=45, cex=4, col=2)
 
 plot0 <- function(x = c(-1, 1), y = NULL, fill = NULL, text = NULL, ...) {
+    ## 
     args <- list(...)
-    coor <- list(x = x, y = y)
+    args_txt <- args[names(args) %in% methods::formalArgs(graphics::text.default)]
     deft <- list(ann = FALSE, axes = FALSE, type = "n")
-    ##--- default behavior for matrix and vectors
+    # default behavior for matrix and vectors
     if (NCOL(as.matrix(x)) > 1 & is.null(y)) {
         if (is.null(y)) {
             y <- x[, 2L]
         }
         x <- x[, 1L]
     } else {
-        if (is.null(y)) {
-            y <- x
-        }
+        if (is.null(y)) 
+            y <- x else stopifnot(length(x) == length(y))
     }
-    
+    # 
+    coor <- list(x = x, y = y)
     
     ## 
     if (length(args) > 0) {
@@ -51,12 +54,13 @@ plot0 <- function(x = c(-1, 1), y = NULL, fill = NULL, text = NULL, ...) {
             deft <- deft[-id]
         do.call(graphics::plot.default, args = as.list(c(coor, args, deft)))
     } else graphics::plot.default(x = x, y = y, ann = FALSE, axes = FALSE, type = "n")
-    ## 
+    
     if (!is.null(fill)) 
         plotAreaColor(color = fill)
     ## 
     if (!is.null(text)) 
-        text(mean(x), mean(y), labels = as.character(text), ...)
+        do.call(graphics::text.default, args = as.list(c(x = mean(x), y = mean(y), 
+            labels = as.character(text), args_txt)))
     ## 
     invisible(NULL)
 }
--- tests/testthat/test-plot0.R
+++ tests/testthat/test-plot0.R
@@ -10,7 +10,19 @@ plot0(x=c(0,1), y=c(0,2), xlim=c(0,10), fill=2)
 pus2 <- par()$usr
 dev.off()
 
+##
+plot0(x=cbind(c(0,1), c(0,2)), text="test")
+pus3 <- par()$usr
+dev.off()
+
+
+
 test_that("test par()$usr dimensions", {
   expect_true(all(pus1 == c(-1.08, 1.08, -1.08, 1.08)))
   expect_true(all(pus2 == c(-0.40, 10.40, -0.08, 2.08)))
+  expect_true(all(pus3 == c(-0.04, 1.04, -0.08, 2.08)))
+})
+
+test_that("expected errors", {
+  expect_error(plot0(1, c(1,1)), "length(x) == length(y) is not TRUE", fixed = T)
 })
