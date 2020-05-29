d89a77e3843973443ff05ab5173ba698ee9fa1c4
Nicolas Casajus <nicolas.casajus@gmail.com>
2020-05-29 18:02:35 +0200

    :boom: New Features: Frame, Grid, and Spatial Axis

---
+++ R/addAxis.R
@@ -0,0 +1,55 @@
+#' Add Spatial Cardinal Directions to Axis Labels
+#'
+#' This function adds cardinal directions to axis labels in a map.
+#'
+#' @param side An integer between 1 (bottom x-Axis) and 4 (right y-Axis).
+#' @param at Values on the axis to add labels.
+#' @param mgp Customize axis positions. See `par()`.
+#' @param digits Number of digits of labels.
+#' @param ... Other parameters to pass to `axis()`.
+#'
+#' @importFrom graphics par axTicks axis
+#' @export
+#'
+#' @keywords axis, spatial
+#'
+#' @author Nicolas CASAJUS, \email{nicolas.casajus@@gmail.com}
+#'
+#' @examples
+#' maps::map()
+#' addFrame(grid = TRUE, add = TRUE, width = 5)
+#' addAxis(1)
+#' addAxis(2, at = seq(-50, 50, 100), digits = 2)
+#' addAxis(3, lwd = 0, lwd.ticks = 0.5)
+#' addAxis(4, las = 1, col.axis = "red")
+
+
+addAxis <- function(side, at = axTicks(side), mgp = par()$mgp, digits = 0,
+                     ...) {
+
+  opar <- par(no.readonly = TRUE)
+
+  options(warn = -1)
+  par(mgp = mgp)
+  options(warn = 0)
+
+  if (side %in% c(1, 3)) {
+
+    labels <- ifelse(at == 0,
+                     sprintf("%.0f\u00B0", at),
+                     ifelse(at > 0,
+                            sprintf(paste0("%.", digits, "f\u00B0E"), at),
+                            sprintf(paste0("%.", digits, "f\u00B0W"), -1 * at)))
+  } else {
+
+    labels <- ifelse(at == 0,
+                     sprintf("%.0f\u00B0", at),
+                     ifelse(at > 0,
+                            sprintf(paste0("%.", digits, "f\u00B0N"), at),
+                            sprintf(paste0("%.", digits, "f\u00B0S"), -1 * at)))
+  }
+
+  axis(side = side, at = at, labels = labels, ...)
+
+  par(opar, no.readonly = TRUE)
+}
---
+++ R/addGrid.R
@@ -0,0 +1,113 @@
+#' Add a Grid to a Plot
+#'
+#' This function adds a grid to an existing plot (like `box()`). User can
+#' specify coordinates on x- and/or y-axis where to draw vertical and/or
+#' horizontal lines respectively. User can also color the background of the plot
+#' area and add a box around this area (if required).
+#'
+#' @param at_x Coordinates on the x-Axis where to draw vertical lines.
+#' @param at_y Coordinates on the y-Axis where to draw horizontal lines.
+#' @param col The color of the background.
+#' @param border The color of lines (and box).
+#' @param lwd The width of lines (see `par()`).
+#' @param lty The type of lines (see `par()`).
+#' @param box A boolean. If `TRUE` add a box around the plot area.
+#' @param ... Other graphical parameters as in `par()`.
+#'
+#' @details
+#' If user does not specify `at_x` and `at_y`, the grid is aligned with the tick
+#' marks on the corresponding default axes (computed by `axTicks`).
+#'
+#' @importFrom graphics par axTicks rect lines
+#' @export
+#'
+#' @keywords box, grid, background
+#'
+#' @author Nicolas CASAJUS, \email{nicolas.casajus@@gmail.com}
+#'
+#' @examples
+#' maps::map()
+#' addGrid()
+#' addGrid(box = TRUE)
+#' addGrid(col = "#ff000044", border = "green", box = TRUE)
+#'
+#' maps::map()
+#' addGrid(at_x = axTicks(1), border = "blue", lwd = 2)
+#' addGrid(at_y = axTicks(2), border = "red")
+#' addGrid(at_x = NULL, at_y = NULL, lwd = 3, box = TRUE)
+
+
+addGrid <- function(at_x, at_y, col = NA, border = "black", lwd = 1, lty = 1,
+                     box = FALSE, ...) {
+
+  opar <- par(no.readonly = TRUE)
+  par(...)
+
+  if (missing(at_x) && missing(at_y)) {
+
+    at_x <- axTicks(side = 1)
+    at_y <- axTicks(side = 2)
+
+  } else {
+
+    if (missing(at_x)) at_x <- NULL
+    if (missing(at_y)) at_y <- NULL
+  }
+
+  if (!is.null(at_x)) {
+
+    at_x <- sort(unique(at_x[at_x > par()$usr[1] & at_x < par()$usr[2]]))
+
+    if (!length(at_x)) stop("x-Axis graduations are ouside plot range.")
+  }
+
+  if (!is.null(at_y)) {
+
+    at_y <- sort(unique(at_y[at_y > par()$usr[3] & at_y < par()$usr[4]]))
+
+    if (!length(at_y)) stop("y-Axis graduations are ouside plot range.")
+  }
+
+  par(xpd = TRUE)
+
+  if (box) {
+
+    rect(par()$usr[1], par()$usr[3], par()$usr[2], par()$usr[4],
+         col = col, border = border, lwd = lwd, lty = lty)
+
+  } else {
+
+    rect(par()$usr[1], par()$usr[3], par()$usr[2], par()$usr[4],
+         col = col, border = NA, lwd = lwd, lty = lty)
+  }
+
+  if (!is.null(at_x)) {
+
+    for (at in at_x) {
+
+      lines(
+        x   = rep(at, 2),
+        y   = c(par()$usr[3], par()$usr[4]),
+        col = border,
+        lwd = lwd,
+        lty = lty
+      )
+    }
+  }
+
+  if (!is.null(at_y)) {
+
+    for (at in at_y) {
+
+      lines(
+        x   = c(par()$usr[1], par()$usr[2]),
+        y   = rep(at, 2),
+        col = border,
+        lwd = lwd,
+        lty = lty
+      )
+    }
+  }
+
+  par(opar, no.readonly = TRUE)
+}
